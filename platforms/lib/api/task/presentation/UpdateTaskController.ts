68ea88cbc7d0b00e516b866d12a9234d598bf1a8
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-07-22 19:04:13 +0900

    remove deprecated code

--- platforms/lib/api/task/presentation/UpdateTaskController.ts
+++ platforms/lib/api/task/presentation/UpdateTaskController.ts
@@ -1,7 +1,5 @@
-import { Task } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { UpdateTaskDoneStatusUseCase } from "../usecase/UpdateTaskDoneStatusUseCase";
-import { UpdateTaskUseCase } from "../usecase/UpdateTaskUseCase";
 
 export class UpdateTaskController {
   async updateDoneStatus(req: NextApiRequest, res: NextApiResponse) {
@@ -31,23 +29,6 @@ export class UpdateTaskController {
         }
       default:
         return res.status(500).end("Invalid request");
-    }
-  }
-
-  async updateTask(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ): Promise<void | NextApiResponse<Task>> {
-    const usecase = new UpdateTaskUseCase();
-    const task = req.body;
-
-    try {
-      const updatedTask = await usecase.updateTask(task);
-
-      return res.status(201).json(updatedTask);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).end(error);
     }
   }
 }
