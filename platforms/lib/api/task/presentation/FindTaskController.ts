3acf975df04f68921800f9b3e65105115ce82730
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-07-24 22:19:51 +0900

    test

--- platforms/lib/api/task/presentation/FindTaskController.ts
+++ platforms/lib/api/task/presentation/FindTaskController.ts
@@ -1,24 +1,24 @@
-import { Task } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next";
-import { Session } from "next-auth";
-import { FindTaskUseCase } from "../usecase/FindTaskUseCase";
+import { Task } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Session } from 'next-auth'
+import { FindTaskUseCase } from '../usecase/FindTaskUseCase'
 
 export class FindTaskController {
   async findAllTasks(
     req: NextApiRequest,
     res: NextApiResponse,
     session: Session
-  ): Promise<void | NextApiResponse<Array<Task>>> {
+  ): Promise<void | NextApiResponse<Task[]>> {
     if (!session.user.id)
-      return res.status(500).end("Server failed to get session user ID");
-    const usecase = new FindTaskUseCase();
+      return res.status(500).end('Server failed to get session user ID')
+    const usecase = new FindTaskUseCase()
 
     try {
-      const tasks = await usecase.findAllTasks(session.user.id);
+      const tasks = await usecase.findAllTasks(session.user.id)
 
-      return res.status(200).json(tasks);
+      return res.status(200).json(tasks)
     } catch (error) {
-      return res.status(500).end(error);
+      return res.status(500).end(error)
     }
   }
 
--- platforms/lib/api/task/usecase/CreateTaskUseCase.ts
+++ platforms/lib/api/task/usecase/CreateTaskUseCase.ts
@@ -1,21 +1,20 @@
-import { Task } from "../domain/Task";
-import { TaskRepository } from "../infrastructure/TaskRepository";
-import { TaskId } from "../domain/TaskId";
-import { TaskName } from "../domain/TaskName";
+import { Task } from '../domain/Task'
+import { TaskRepository } from '../infrastructure/TaskRepository'
+import { TaskName } from '../domain/TaskName'
 
 export class CreateTaskUseCase {
-  taskRepository: TaskRepository = new TaskRepository();
+  taskRepository: TaskRepository = new TaskRepository()
 
   async execute(name: string, userId: string): Promise<Task> {
-    const taskName = new TaskName(name);
-    const task = Task.create(taskName, userId);
+    const taskName = new TaskName(name)
+    const task = Task.create(taskName, userId)
     try {
-      this.taskRepository.create(task);
+      this.taskRepository.create(task)
     } catch (error) {
-      console.error(error);
-      throw error;
+      console.error(error)
+      throw error
     }
 
-    return task;
+    return task
   }
 }
