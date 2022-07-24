7b96a873dd73b0a2cc9b6c78a538c6d1be9a60e9
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-07-25 00:43:54 +0900

    Add Dto

--- platforms/lib/api/task/usecase/CreateTaskUseCase.ts
+++ platforms/lib/api/task/usecase/CreateTaskUseCase.ts
@@ -5,7 +5,7 @@ import { TaskName } from '../domain/TaskName'
 export class CreateTaskUseCase {
   taskRepository: TaskRepository = new TaskRepository()
 
-  async execute(name: string, userId: string): Promise<Task> {
+  async execute(name: string, userId: string): Promise<CreateTaskDtoType> {
     const taskName = new TaskName(name)
     const task = Task.create(taskName, userId)
     try {
@@ -15,6 +15,30 @@ export class CreateTaskUseCase {
       throw error
     }
 
-    return task
+    return new CreateTaskDto(task)
+  }
+}
+
+export interface CreateTaskDtoType {
+  id: string
+  name: string
+  done: boolean
+  removed: boolean
+  userId: string
+}
+
+class CreateTaskDto implements CreateTaskDtoType {
+  id: string
+  name: string
+  done: boolean
+  removed: boolean
+  userId: string
+
+  constructor(task: Task) {
+    this.id = task.id.value
+    this.name = task.name.value
+    this.done = task.done
+    this.removed = task.removed
+    this.userId = task.userId
   }
 }
--- platforms/lib/api/task/usecase/FindTaskUseCase.ts
+++ platforms/lib/api/task/usecase/FindTaskUseCase.ts
@@ -1,14 +1,42 @@
-import { TaskId } from "../domain/TaskId";
-import { TaskRepository } from "../infrastructure/TaskRepository";
+import { Task } from '../domain/Task'
+import { TaskId } from '../domain/TaskId'
+import { TaskRepository } from '../infrastructure/TaskRepository'
 
 export class FindTaskUseCase {
-  taskRepository: TaskRepository = new TaskRepository();
+  taskRepository: TaskRepository = new TaskRepository()
 
-  async findTaskById(taskId: string) {
-    return await this.taskRepository.findById(new TaskId(taskId));
+  async findTaskById(taskId: string): Promise<FindTaskDtoType | null> {
+    const task = await this.taskRepository.findById(new TaskId(taskId))
+
+    return task ? new FindTaskDto(task) : null
   }
 
-  async findAllTasks(userId: string) {
-    return await this.taskRepository.findAll(userId);
+  async findAllTasks(userId: string): Promise<FindTaskDtoType[]> {
+    const tasks = await this.taskRepository.findAll(userId)
+
+    return tasks.map((task) => new FindTaskDto(task))
+  }
+}
+
+export interface FindTaskDtoType {
+  id: string
+  name: string
+  done: boolean
+  removed: boolean
+  userId: string
+}
+class FindTaskDto implements FindTaskDtoType {
+  id: string
+  name: string
+  done: boolean
+  removed: boolean
+  userId: string
+
+  constructor(task: Task) {
+    this.id = task.id.value
+    this.name = task.name.value
+    this.done = task.done
+    this.removed = task.removed
+    this.userId = task.userId
   }
 }
