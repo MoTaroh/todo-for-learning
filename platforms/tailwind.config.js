ad5679b67f2a962512b2d204667827efede59c13
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-08-14 00:27:11 +0900

    fix

--- platforms/tailwind.config.js
+++ platforms/tailwind.config.js
@@ -1,72 +1,75 @@
-import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
 
-export const content = [
-  './pages/**/*.{js,ts,jsx,tsx}',
-  './components/**/*.{js,ts,jsx,tsx}',
-  './lib/**/*.{js,ts,jsx,tsx}',
-];
-export const darkMode = 'media';
-export const theme = {
-  fontFamily: {
-    cal: ['Cal Sans', 'Inter var', 'sans-serif'],
-  },
-  extend: {
-    colors: {
-      current: 'currentColor',
-    },
-    width: {
-      1536: '1536px',
-    },
-    height: {
-      150: '37.5rem',
-    },
-    margin: {
-      30: '7.5rem',
-    },
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'media',
+  theme: {
     fontFamily: {
-      sans: ['Inter var', ..._fontFamily.sans],
-      mono: ['Consolas', ..._fontFamily.mono],
+      cal: ['Cal Sans', 'Inter var', 'sans-serif'],
     },
-    typography: {
-      DEFAULT: {
-        css: {
-          h1: {
-            fontFamily: 'Cal Sans',
+    extend: {
+      colors: {
+        current: 'currentColor',
+      },
+      width: {
+        1536: '1536px',
+      },
+      height: {
+        150: '37.5rem',
+      },
+      margin: {
+        30: '7.5rem',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        mono: ['Consolas', ...defaultTheme.fontFamily.mono],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              fontFamily: 'Cal Sans',
+            },
+            h2: {
+              fontFamily: 'Cal Sans',
+            },
+            h3: {
+              fontFamily: 'Cal Sans',
+            },
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
           },
-          h2: {
-            fontFamily: 'Cal Sans',
-          },
-          h3: {
-            fontFamily: 'Cal Sans',
-          },
-          'blockquote p:first-of-type::before': { content: 'none' },
-          'blockquote p:first-of-type::after': { content: 'none' },
         },
       },
-    },
-    keyframes: {
-      wiggle: {
-        '0%, 100%': {
-          transform: 'translateX(0%)',
-          transformOrigin: '50% 50%',
+      keyframes: {
+        wiggle: {
+          '0%, 100%': {
+            transform: 'translateX(0%)',
+            transformOrigin: '50% 50%',
+          },
+          '15%': { transform: 'translateX(-6px) rotate(-6deg)' },
+          '30%': { transform: 'translateX(9px) rotate(6deg)' },
+          '45%': { transform: 'translateX(-9px) rotate(-3.6deg)' },
+          '60%': { transform: 'translateX(3px) rotate(2.4deg)' },
+          '75%': { transform: 'translateX(-2px) rotate(-1.2deg)' },
         },
-        '15%': { transform: 'translateX(-6px) rotate(-6deg)' },
-        '30%': { transform: 'translateX(9px) rotate(6deg)' },
-        '45%': { transform: 'translateX(-9px) rotate(-3.6deg)' },
-        '60%': { transform: 'translateX(3px) rotate(2.4deg)' },
-        '75%': { transform: 'translateX(-2px) rotate(-1.2deg)' },
       },
-    },
-    animation: {
-      wiggle: 'wiggle 0.8s both',
-    },
-    backgroundImage: {
-      'blank-task': "url('/images/task_blank.png')",
+      animation: {
+        wiggle: 'wiggle 0.8s both',
+      },
+      backgroundImage: {
+        'blank-task': "url('/images/task_blank.png')",
+      },
     },
   },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+  ],
 };
-export const plugins = [
-  require('@tailwindcss/typography'),
-  require('@tailwindcss/forms'),
-  require('@tailwindcss/line-clamp'),
-];
