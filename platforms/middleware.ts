1eae2ded001a62e424b1a65f0e8e5dc5f7e70eba
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-08-14 23:04:53 +0900

    redirect to /tasks

--- platforms/middleware.ts
+++ platforms/middleware.ts
@@ -1,12 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
   matcher: [
-    "/",
-    "/([^/.]*)", // exclude `/public` files by matching all paths except for paths containing `.` (e.g. /logo.png)
-    "/site/:path*",
-    "/post/:path*",
-    "/_sites/:path*",
+    '/',
+    '/([^/.]*)', // exclude `/public` files by matching all paths except for paths containing `.` (e.g. /logo.png)
+    '/site/:path*',
+    '/post/:path*',
+    '/_sites/:path*',
+    '/tasks/:path*',
   ],
 };
 
@@ -14,32 +15,27 @@ export default function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers.get("host") || "demo.vercel.pub";
-
-  // Only for demo purposes – remove this if you want to use your root domain as the landing page
-  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://demo.vercel.pub");
-  }
+  const hostname = req.headers.get('host') || 'demo.vercel.pub';
 
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
       in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
       still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
   const currentHost =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+    process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
       ? hostname
-          .replace(`.vercel.app`, "")
-          .replace(`.platformize.vercel.app`, "")
-      : hostname.replace(`.localhost:3000`, "");
+          .replace(`.vercel.app`, '')
+          .replace(`.platformize.vercel.app`, '')
+      : hostname.replace(`.localhost:3000`, '');
 
   // rewrites for app pages
-  if (currentHost == "app" || currentHost.startsWith(process.env.APP_HOST)) {
+  if (currentHost == 'app' || currentHost.startsWith(process.env.APP_HOST)) {
     if (
-      url.pathname === "/login" &&
-      (req.cookies.get("next-auth.session-token") ||
-        req.cookies.get("__Secure-next-auth.session-token"))
+      url.pathname === '/login' &&
+      (req.cookies.get('next-auth.session-token') ||
+        req.cookies.get('__Secure-next-auth.session-token'))
     ) {
-      url.pathname = "/";
+      url.pathname = '/tasks';
       return NextResponse.redirect(url);
     }
 
@@ -48,7 +44,7 @@ export default function middleware(req: NextRequest) {
   }
 
   // rewrite root application to `/home` folder (for LandingPage)
-  if (hostname === "localhost:3000" || hostname === "platformize.vercel.app") {
+  if (hostname === 'localhost:3000' || hostname === 'platformize.vercel.app') {
     url.pathname = `/home${url.pathname}`;
     return NextResponse.rewrite(url);
   }
