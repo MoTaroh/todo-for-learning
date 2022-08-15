436f9b4bb1b166f3a3b338daadc143fda326285c
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-08-15 19:24:58 +0900

    fix error message

--- platforms/lib/api/task/domain/TaskName.ts
+++ platforms/lib/api/task/domain/TaskName.ts
@@ -5,7 +5,7 @@ export class TaskName {
   constructor(name: string) {
     if (0 === name.length || name.length > this.TASK_NAME_MAX_LENGTH) {
       throw new Error(
-        `タスク名は0文字以上、${this.TASK_NAME_MAX_LENGTH}文字以下で入力してください`
+        `タスク名は1文字以上、${this.TASK_NAME_MAX_LENGTH}文字以下で入力してください`
       );
     }
     this.value = name;
--- platforms/lib/api/task/domain/test/TaskName.test.ts
+++ platforms/lib/api/task/domain/test/TaskName.test.ts
@@ -14,13 +14,13 @@ describe('ValueObject: TaskName', () => {
     // when & then
     expect(
       () => new TaskName('This is very long task name so we can not assign.')
-    ).toThrow('タスク名は0文字以上、20文字以下で入力してください');
+    ).toThrow('タスク名は1文字以上、20文字以下で入力してください');
   });
 
   test('0文字の値を渡すと、例外が発生する', () => {
     // when & then
     expect(() => new TaskName('')).toThrow(
-      'タスク名は0文字以上、20文字以下で入力してください'
+      'タスク名は1文字以上、20文字以下で入力してください'
     );
   });
 });
