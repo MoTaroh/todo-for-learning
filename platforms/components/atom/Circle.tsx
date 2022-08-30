15e07665a49643a97bae055c8e017e4eb981d49f
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-08-30 19:39:28 +0900

    introduce atom component

---
+++ platforms/components/atom/Circle.tsx
@@ -0,0 +1,16 @@
+import { ColorUnion } from '@/types/colorUnion';
+
+interface Props {
+  size: 'small' | 'big';
+  color: ColorUnion;
+}
+
+export default function Circle({ size, color }: Props) {
+  return (
+    <div
+      className={`bg-${color}-600 rounded-full ${
+        size === 'small' ? 'w-3 h-3' : 'w-5 h-5'
+      }`}
+    ></div>
+  );
+}
